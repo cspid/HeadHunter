1bc50bb1237c88dce7ab5c2f083f561373ffe8b6
OverG0d <ardainan196@gmail.com>
2019-02-07 15:57:13 -0500

    Created a script for raycast that is able to identify player.

--- Assets/Arda/TransformTarget.cs
+++ Assets/Arda/TransformTarget.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class TransformTarget : MonoBehaviour
 {
-
-    public Transform direction;
+    public Transform playerPosition;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -15,23 +15,34 @@ public class TransformTarget : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        RaycastHit hit;
-        // Does the ray intersect any objects excluding the player layer
-        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity))
-        {
-            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
-            Debug.Log("Did Hit");
-        }
-        else
-        {
-            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.white);
-            Debug.Log("Did not Hit");
-        }
+        Direction(playerPosition.position);
 
     }
 
-    public bool Direction(Transform direction)
+    public bool Direction(Vector3 playerPos)
     {
-        return true;
+
+        Vector3 dir = playerPos - this.transform.position;
+        dir.Normalize();
+
+
+        RaycastHit hit;
+        // Does the ray intersect any objects excluding the player layer
+        if (Physics.Raycast(transform.position, dir, out hit, Mathf.Infinity))
+        {
+            if (hit.collider.tag == "Player")
+            {
+                Debug.DrawRay(transform.position, dir * hit.distance, Color.yellow);
+                Debug.Log("Player Hit");
+                return true;
+            }
+        }
+
+            Debug.DrawRay(transform.position, dir * 1000, Color.white);
+            Debug.Log("Did not Hit");
+        return false;
+
+
+
     }
 }
