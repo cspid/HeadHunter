bd6900b17c0abed1d02eb80cbde2f3e3565f9a80
cspidla <carlospidla@gmail.com>
2019-02-24 13:45:44 -0500

    Added Recoil, Firing

--- Assets/Gun.cs
+++ Assets/Gun.cs
@@ -8,10 +8,11 @@ public class Gun : MonoBehaviour
     public Transform firePoint;
     bool targeted;
     public Color lineColor;
+    public Animator recoilAnimator;
 
     // Update is called once per frame
     void Update()
-    {
+    
         {
             RaycastHit hit;
             var ray = new Ray(firePoint.position, firePoint.forward);
@@ -30,6 +31,12 @@ public class Gun : MonoBehaviour
 
             Vector3 forward = firePoint.TransformDirection(Vector3.forward) * 10;
             Debug.DrawRay(firePoint.position, forward, lineColor);
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            print("fire");
+            recoilAnimator.SetTrigger("Go");
         }
     }
+
 }
