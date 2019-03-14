c40835557169b04ffaf90d116f8456be9e3ab0a3
Erdem Gunay <merdemgunay88@gmail.com>
2019-03-14 14:59:45 -0400

    Stuff from the other branch

---
+++ Assets/Scripts/EnemyStates.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyStates : MonoBehaviour
+{
+
+    public enum State { Stealth, Attack, Cover, Death };
+    public State state;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (state)
+        {
+            case State.Stealth:
+                Stealth();
+                break;
+            case State.Attack:
+                Attack();
+                break;
+            case State.Cover:
+                Cover();
+                break;
+            case State.Death:
+                Death();
+                break;
+
+        }
+    }
+
+    void Stealth()
+    {
+        Debug.Log("Stealth");
+    }
+    void Attack()
+    {
+        Debug.Log("Attack");
+    }
+    void Cover()
+    {
+        Debug.Log("Cover");
+    }
+    void Death()
+    {
+        Debug.Log("Death");
+    }
+}
