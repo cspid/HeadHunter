abe1c0396d8f19f8c3b143018c48cb0e6d350ee1
Erdem Gunay <merdemgunay88@gmail.com>
2019-04-06 16:27:56 -0400

    weapon system rework

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,12 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Enemy : MonoBehaviour
 {
     [SerializeField] Transform aimPos;
     float suppression = 0;
     float suppressionNormSpeed = 0.1f;
+
+    [SerializeField] TextMeshProUGUI suppText;  //Placeholder stuff
+    [SerializeField] Transform flankCheckPos;   //This should be at the bottom of the enemy
 
     // Start is called before the first frame update
     void Start()
@@ -26,6 +30,8 @@ public class Enemy : MonoBehaviour
                 suppression = 0;
             }
         }
+
+        suppText.text = suppression.ToString();
     }
 
     // IMPLEMENT DMG LATER
@@ -39,8 +45,25 @@ public class Enemy : MonoBehaviour
         return aimPos;
     }
 
+    public bool isFlanked(Vector3 gunCheckPos, GameObject attacker)
+    {
+        RaycastHit hit;
+        Debug.DrawRay(flankCheckPos.position, gunCheckPos - flankCheckPos.position, Color.green);
+
+        if (Physics.Raycast(flankCheckPos.position, gunCheckPos - flankCheckPos.position, out hit, Mathf.Infinity))
+        {
+            if (hit.transform.gameObject == attacker)
+            {
+                Debug.Log("flanked.");
+                return true;
+            }
+        }
+        return false;
+    }
+
     public void getSupressed(float amount)
     {
+        Debug.Log("Getting suppressed by: " + amount);
         suppression += amount;
 
         if (suppression > 1)
---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    [SerializeField] GameObject target;
+
+    float MAX_SUPRESS_DISTANCE = 10f;
+    float MAX_SUPPRESSION_PERC = .9f;
+
+    bool isShooting = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isShooting && Input.GetAxis("Fire1") > 0.2f)
+        {
+            isShooting = true;
+            //startShooting();
+            shoot();
+        }
+        else if (isShooting && Input.GetAxis("Fire1") < 0.2f)
+        {
+            isShooting = false;
+        }
+    }
+
+    void shoot()
+    {
+        Debug.Log("Pew pew");
+        supress(this.transform.position, target.transform.position - this.transform.position);
+
+        if (target.GetComponent<Enemy>())
+        {
+            if (target.GetComponent<Enemy>().isFlanked(this.transform.position, this.gameObject))
+            {
+                target.GetComponent<Enemy>().takeDamage();
+            }
+            
+        }
+    }
+
+    void setTarget(GameObject tar)
+    {
+        target = tar;
+    }
+
+    void supress(Vector3 linePnt, Vector3 lineDir)
+    {
+        Enemy[] enemies = FindObjectsOfType<Enemy>();
+
+        foreach (Enemy enemy in enemies)
+        {
+            if (Vector3.Distance(enemy.transform.position, target.transform.position) <= MAX_SUPRESS_DISTANCE)
+            {
+                enemy.getSupressed(MAX_SUPPRESSION_PERC * (MAX_SUPRESS_DISTANCE - Vector3.Distance(enemy.transform.position, target.transform.position)) / MAX_SUPRESS_DISTANCE);
+            }
+        }
+
+        //foreach (Enemy enemy in enemies)
+        //{
+        //    Vector3 nearestPos = NearestPointOnLine(linePnt, lineDir, enemy.transform.position);
+        //    if (Vector3.Distance(nearestPos, enemy.transform.position) <= MAX_SUPRESS_DISTANCE) // Close enough to supress
+        //    {
+        //        enemy.getSupressed(MAX_SUPPRESSION_PERC * (Vector3.Distance(nearestPos, enemy.transform.position) / MAX_SUPRESS_DISTANCE));
+        //    }
+
+        //}
+    }
+
+    public static Vector3 NearestPointOnLine(Vector3 linePnt, Vector3 lineDir, Vector3 pnt)
+    {
+        lineDir.Normalize();//this needs to be a unit vector
+        var v = pnt - linePnt;
+        var d = Vector3.Dot(v, lineDir);
+        return linePnt + lineDir * d;
+    }
+}
