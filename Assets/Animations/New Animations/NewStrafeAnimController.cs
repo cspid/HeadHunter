0dea6932a13be8ee6a5aa5a0e31343402a197b22
SJH607-11W\Graphic <carlospidla@gmail.com>
2019-04-09 18:59:19 -0400

    hit objects

---
+++ Assets/Animations/New Animations/NewStrafeAnimController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NewStrafeAnimController : MonoBehaviour {
+
+	Animator animator;
+	public NavMeshAgent myAgent;
+	Rigidbody rb;
+	public float animSpeed = 1.5f;
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator>();
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		animator.speed = animSpeed;
+		if(rb.isKinematic == false)
+		{
+			animator.SetFloat("Forward", transform.InverseTransformDirection(rb.velocity).z);
+            animator.SetFloat("Right", transform.InverseTransformDirection(rb.velocity).x);
+		}
+		else if (myAgent != null)
+		{
+			animator.SetFloat("Forward", transform.InverseTransformDirection(myAgent.velocity).z);
+			animator.SetFloat("Right", transform.InverseTransformDirection(myAgent.velocity).x);
+		}
+		//print(rb.velocity.x);
+
+
+	}
+}
