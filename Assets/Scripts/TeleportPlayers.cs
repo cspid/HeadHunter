423b0fb595ee2f3f778db67de2fba91c87422932
rasterradio <funnyguy150@gmail.com>
2019-04-09 16:00:53 -0400

    Start of elevator teleport

---
+++ Assets/Scripts/TeleportPlayers.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportPlayers : MonoBehaviour
+{
+    public Transform teleDestination;
+
+    private void Awake()
+    {
+        teleDestination = GetComponent<Transform>();
+    }
+
+    void Start()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        other.transform.position = teleDestination.position;
+        //need to make it check for both players to be in the elevator first
+    }
+}
