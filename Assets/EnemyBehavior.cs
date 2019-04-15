002466c548e5ed16df3cff795ee91fe40565b745
Erdem Gunay <merdemgunay88@gmail.com>
2019-04-15 16:26:29 -0400

    enemy line of sight

--- Assets/EnemyBehavior.cs
+++ Assets/EnemyBehavior.cs
@@ -213,9 +213,17 @@ public class EnemyBehavior : MonoBehaviour
     void shoot()
     {
         PlayerDanger targetPlayer = findTarget();
-        Debug.Log("take that you evil player character!");
+        //Debug.Log("take that you evil player character!");
 
-        targetPlayer.getSuppressed(suppressionValue, targetPlayer.isFlanked(gunCheckPos.position, this.transform.parent.gameObject));
+        RaycastHit hit;
+        Physics.Raycast(GetComponentInParent<Enemy>().getEyePos().position, 
+            targetPlayer.getEyePos().position - GetComponentInParent<Enemy>().getEyePos().position, out hit, Mathf.Infinity);
+
+        if (hit.transform.IsChildOf(targetPlayer.transform) || hit.transform.gameObject == targetPlayer.transform || hit.transform == targetPlayer.transform.parent)
+        {
+            targetPlayer.getSuppressed(suppressionValue, targetPlayer.isFlanked(gunCheckPos.position, this.transform.parent.gameObject));
+        }
+            
         muzzleFlash.Play();
 
     }
@@ -230,7 +238,17 @@ public class EnemyBehavior : MonoBehaviour
                 return player;
             }
         }
+        foreach (PlayerDanger player in players)
+        {
+            RaycastHit hit;
+            Physics.Raycast(GetComponentInParent<Enemy>().getEyePos().position,
+            player.getEyePos().position - GetComponentInParent<Enemy>().getEyePos().position, out hit, Mathf.Infinity);
+            if (hit.transform.IsChildOf(player.transform) || hit.transform.gameObject == player.transform || hit.transform == player.transform.parent)
+            {
+                return player;
+            }
 
+        }
         return players[Random.Range(0, players.Length)];
     }
 
