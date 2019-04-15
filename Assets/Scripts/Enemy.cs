00cca3b28ea2d26102a9c515312dacb35cc66f9c
Erdem Gunay <merdemgunay88@gmail.com>
2019-04-15 13:51:40 -0400

    insta kill

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -92,6 +92,18 @@ public class Enemy : MonoBehaviour
         }
     }
 
+    public void die()
+    {
+        if (GetComponentInChildren<PuppetMaster>())
+        {
+            Debug.Log("enemy ded.");
+            GetComponentInChildren<PuppetMaster>().state = PuppetMaster.State.Dead;
+            Destroy(GetComponentInChildren<EnemyBehavior>());
+            Destroy(canvas);
+            Destroy(this);
+        }
+    }
+
     public Transform getAimPos()
     {
         return aimPos;
@@ -101,7 +113,7 @@ public class Enemy : MonoBehaviour
     {
         RaycastHit hit;
         Physics.Raycast(gunCheckPos - flankCheckPos.position, flankCheckPos.position, out hit, Mathf.Infinity);
-        Debug.Log("flank check: :" + hit.transform.gameObject + " " + hit.transform.parent.gameObject + "Compared to attacker: " + attacker);
+        //Debug.Log("flank check: :" + hit.transform.gameObject + " " + hit.transform.parent.gameObject + "Compared to attacker: " + attacker);
         //if (Physics.Raycast(gunCheckPos - flankCheckPos.position, flankCheckPos.position, out hit, Mathf.Infinity))
         Debug.DrawRay(gunCheckPos, flankCheckPos.position - gunCheckPos, Color.red, 1.5f);
 
